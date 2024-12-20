335c6761615e9bb6c61b957cd36efe5cd46b035b
Ean Seng Chang <eansengchang@gmail.com>
2024-12-20 13:12:32 +0000

    ESC Version

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,13 +1,13 @@
 <template>
-  <p>Desktop Postflop v0.2.7</p>
+  <p>Desktop Postflop v0.2.7 - ESC Version</p>
   <p>Copyright (C) 2022 Wataru Inariba</p>
   <p>
     <a
       target="_blank"
       class="link"
-      href="https://github.com/b-inary/desktop-postflop"
+      href="https://github.com/eansengchang/desktop-postflop"
     >
-      https://github.com/b-inary/desktop-postflop
+      https://github.com/eansengchang/desktop-postflop
     </a>
   </p>
 </template>
